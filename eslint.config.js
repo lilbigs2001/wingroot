e5dde966eea2c843b318cd8be1a8c3655c320360
lilbigs2001 <joebigelow@protonmail.com>
2025-10-03 21:26:51 -0500

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-// https://docs.expo.dev/guides/using-eslint/
+// eslint.config.js
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const prettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
-  eslintPluginPrettierRecommended,
+  prettierRecommended,
   {
-    ignores: ["dist/*"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ["dist/*", "node_modules/*"],
+    rules: {},
   },
 ]);
