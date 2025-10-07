4d44c5db72d45d9ba8030d900c6486fc98162591
lilbigs2001 <joebigelow@protonmail.com>
2025-10-06 19:57:38 -0500

    Keep selected radio selected when pressed again

--- __tests__/components/RadioButtons-test.tsx
+++ __tests__/components/RadioButtons-test.tsx
@@ -2,11 +2,28 @@ import { RadioButtons } from "@/components/RadioButtons";
 import { YES } from "@/constants";
 import { render, screen, userEvent } from "@testing-library/react-native";
 
+/*
+  - Selecting selected button does not unselect it
+  - Renders multiple buttons
+  - Only selects one button at a time
+*/
+
 it("selecting radio button checks it", async () => {
   const user = userEvent.setup();
   render(<RadioButtons options={[YES]} />);
   const yesRadio = screen.getByRole("radio", { name: YES });
   expect(yesRadio).not.toBeChecked();
+  await user.press(yesRadio);
+  expect(screen.getByRole("radio", { name: YES })).toBeChecked();
+});
+
+it("does not unselect selected button if re-pressed", async () => {
+  const user = userEvent.setup();
+  render(<RadioButtons options={[YES]} />);
+
+  await user.press(screen.getByRole("radio", { name: YES }));
+  const yesRadio = screen.getByRole("radio", { name: YES });
+  expect(yesRadio).toBeChecked();
   await user.press(yesRadio);
   expect(screen.getByRole("radio", { name: YES })).toBeChecked();
 });
